ae98e431f017bf5088356ef035d2ea72c0578b98
verrixkio <verrixkio@gmail.com>
2019-02-13 01:14:37 +0000

    Display shortID and file concerning url_showing it

--- express_server.js
+++ express_server.js
@@ -29,3 +29,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params.shortURL)
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(templateVars)
+  res.render("urls_show", templateVars);
+});
