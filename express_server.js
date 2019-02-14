2fa5ff7ea9ab240e0fae3cdc66e081c0e4afed7f
verrixkio <verrixkio@gmail.com>
2019-02-14 20:31:58 +0000

    set up route for register POST, added the values from register to an object containing users, generate a cookie relating to the username with the randomID made.

--- express_server.js
+++ express_server.js
@@ -18,65 +18,91 @@ const users = {
   }
 }
 
+//Body Parser for our requests
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Setting our server to route and call our ejs files in view.
 app.set("view engine", "ejs");
 
+
+//Set up register Path ********* w2d4
+
+app.get("/register", (req, res) => {
+  //Lets see what we're posting.
+  res.render("urls_register");
+});
+
+app.post("/register", (req, res) => {
+  //Add a new user to the global users object
+  //Access the email
+  let userEmail = req.body.email;
+  //Access the password
+  let userPass = req.body.password;
+  //Get a random number
+  let newId = generateRandomString();
+  //Add these features to the users object
+  users[newId] = {id: newId, email: userEmail, password: userPass};
+  res.cookie("username", newId);
+  
+  res.redirect("/urls");
+});
+
+
+//****************
+
+//Initial Database for our urls and testing
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//Main root page with hello
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//Set up our server to listen for prompts
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//Display a json object concerning our initial database.
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//Testing our .get to demonstrate how our send function works.
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Setting up the main TinyUrl page and renders our urls_index.ejs page.
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
+//Our new url page and renders urls_new.ejs
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_new", templateVars);
 });
 
-
+// Short URL logic for proper redirects.
 app.get("/u/:shortURL", (req, res) => {
   let actualDirect = urlDatabase[req.params.shortURL]
   console.log(actualDirect)
   res.redirect(actualDirect);
-
-  
 });
+
+//add a shortURL ID to our urldatabase if we recieve the prompt.
 app.post("/urls/:shortURL", (req, res) => {
   let shortU = req.params.shortURL
   urlDatabase[shortU] = req.body.longURL
   
   //Update the urldatabase to correct for the input
-  
   res.redirect('/urls');
-});
-
-//Set up register Path
-app.get("/urls/register", (req, res) => {
-  //Lets see what we're posting.
-  console.log(req.params)
-  //let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
-  res.render("urls_register");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -112,7 +138,6 @@ app.post("/logout", (req, res) => {
   res.clearCookie("username")
   res.redirect("/urls")
 });
-
 
 
 function generateRandomString() {
